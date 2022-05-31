7a373db82176284a87e27ddc651e304865fb0e12
pranjal1805 <84785894+pranjal1805@users.noreply.github.com>
2022-06-01 03:22:42 +0530

    Create Fonts.js

---
+++ Code/Fonts.js
@@ -0,0 +1,17 @@
+require("FontCopasetic40x58Numeric").add(Graphics);
+g.setFont("Copasetic40x58Numeric");
+
+require("FontCherry6x10").add(Graphics);
+g.setFont("Cherry6x10");
+
+require("FontDylex7x13").add(Graphics);
+g.setFont("Dylex7x13");
+
+require("FontHaxorNarrow7x17").add(Graphics);
+g.setFont("HaxorNarrow7x17");
+
+require("FontTeletext10x18Mode7").add(Graphics);
+g.setFont("Teletext10x18Mode7");
+
+require("FontTeletext5x9Mode7").add(Graphics);
+g.setFont("Teletext5x9Mode7");
